de07fbb6f9207b6911f6928c6d64a20a8a341e8b
Gondlir <kaoeskt@hotmail.com>
2021-02-14 05:17:58 -0300

    update command task created

---
+++ Todo.Domain/Commands/Contracts/UpdateCommand.cs
@@ -0,0 +1,33 @@
+using System;
+using Flunt.Validations;
+using Flunt.Notifications;
+using Todo.Domain.Commands.Contracts;
+namespace Todo.Domain.Commands
+{
+    public class UpdateCommand : Notifiable, ICommand
+    {
+        public UpdateCommand() { }
+        public UpdateCommand(Guid id, string title, string user)
+        {
+            Id = id;
+            Title = title;
+            User = user;
+        }
+
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string User { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(
+                            new Contract()
+                            .Requires()
+                            .HasMinLen(Title, 3, "Title", "Tarefa com pouca informação !")
+                            .HasMinLen(User, 6, "User", "Usuário inválido")
+                            );
+        }
+    }
+}
