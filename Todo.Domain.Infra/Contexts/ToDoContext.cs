047c77902afdf176cc16debc33f0f4de9cca6e12
Gondlir <kaoeskt@hotmail.com>
2021-02-14 19:06:27 -0300

    added entity framework design

--- Todo.Domain.Infra/Contexts/ToDoContext.cs
+++ Todo.Domain.Infra/Contexts/ToDoContext.cs
@@ -11,5 +11,15 @@ namespace Todo.Domain.Infra.Contexts
 
         }
         public DbSet<ToDoEntity> Todos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ToDoEntity>().ToTable("Todo");
+            modelBuilder.Entity<ToDoEntity>().Property(x => x.Id);
+            modelBuilder.Entity<ToDoEntity>().Property(x => x.RefUser).HasMaxLength(120).HasColumnType("varchar(120)");
+            modelBuilder.Entity<ToDoEntity>().Property(x => x.Title).HasMaxLength(160).HasColumnType("varchar(160)");
+            modelBuilder.Entity<ToDoEntity>().Property(x => x.IsDone).HasColumnType("bit");
+            modelBuilder.Entity<ToDoEntity>().Property(x => x.Date);
+            modelBuilder.Entity<ToDoEntity>().HasIndex(b => b.RefUser);
+        }
     }
 }
