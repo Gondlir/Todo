92802e1e1eeca969326c576ecbccbcf947265e52
Gondlir <kaoeskt@hotmail.com>
2021-02-14 05:12:51 -0300

    Mark as undone task created

---
+++ Todo.Domain/Commands/Contracts/MarkAsUndoneCommand.cs
@@ -0,0 +1,24 @@
+using System;
+using Flunt.Validations;
+using Flunt.Notifications;
+using Todo.Domain.Commands.Contracts;
+namespace Todo.Domain.Commands
+{
+    public class MarkAsUndoneCommand : Notifiable, ICommand
+    {
+        public MarkAsUndoneCommand() { }
+        public MarkAsUndoneCommand(Guid id, string user)
+        {
+            Id = id;
+            User = user;
+        }
+
+        public Guid Id { get; set; }
+        public string User { get; set; }
+        public void Validate()
+        {
+            AddNotifications(new Contract().Requires()
+            .HasMinLen(User, 6, "User", "Usuário inválido !"));
+        }
+    }
+}
