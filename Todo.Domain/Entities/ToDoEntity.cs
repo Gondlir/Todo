95e08c6f4a54f61d122f48e51b6b5b40a85615ed
Gondlir <kaoeskt@hotmail.com>
2021-02-14 04:15:33 -0300

    Creating ToDo item entity

---
+++ Todo.Domain/Entities/ToDoEntity.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Todo.Domain.Entities
+{
+    public class ToDoEntity : Entity
+    {
+        public ToDoEntity(string title, bool isDone, DateTime date, string refUser)
+        {
+            Title = title;
+            IsDone = false;
+            Date = date;
+            RefUser = refUser;
+        }
+
+        public string Title { get; private set; }
+        public bool IsDone { get; private set; }
+        public DateTime Date { get; private set; }
+        public string RefUser { get; private set; }
+
+        public void MarkAsDone()
+        {
+            IsDone = true;
+        }
+        public void MarkAsUnDone()
+        {
+            IsDone = true;
+        }
+        public void UpdateTitle(string title)
+        {
+            Title = title;
+            //validação
+        }
+    }
+}
