a735ea1863fdcdbe07db265fdab2545c166d358a
Gondlir <kaoeskt@hotmail.com>
2021-02-14 16:45:18 -0300

    handler test

---
+++ Todo.Domain.Tests/HandlerTests/CreateHandlerTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+namespace Todo.Domain.HandlerTests
+{
+    [TestClass]
+    public class CreateHandlerTests
+    {
+        [TestMethod]
+        public void GiveInvalidImputInterruptExecition()
+        {
+            Assert.Fail();
+        }
+        [TestMethod]
+        public void GiveValidImputCreateTask()
+        {
+            Assert.Fail();
+        }
+    }
+}
